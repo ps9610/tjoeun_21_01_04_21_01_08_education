e06129e31069fe902a533a0ea312035fd9a201eb
ps9610 <ps961001@gmail.com>
2021-01-06 21:44:22 +0900

    혼자하기

--- 01_06/01_06_프로젝트_제이슨여행사_혼자하기/js/jason.js
+++ 01_06/01_06_프로젝트_제이슨여행사_혼자하기/js/jason.js
@@ -1,113 +1,105 @@
 ;(function($,window,document,undefined){
-    var jason = {
+    var agency = {
         init : function(){
-            var that = this;
-                that.headerFn();
-                that.section1Fn();
-                that.section2Fn();
-                that.section3Fn();
-                that.section4Fn();
-                that.section5Fn();
-                that.footerFn();
+            var _this = this;
+                _this.headerFn();
+                _this.section1Fn();
+                _this.section2Fn();
+                _this.section3Fn();
+                _this.section4Fn();
+                _this.section5Fn();
         },
-        headerFn   : function(){
-            var $this = null;
-            var $header = $("#header");
-            var $window = $(window);
-            var $scroll = false; //스크롤 여부를 알려주는 변수
-            
-            //마우스 오버시 헤더스타일 바뀌기
-            $header.on({
+        headerFn : function(){
+            var _header = $("#header");
+            var _window = $(window);
+            var t = false;
+
+            _header.on({
                 mouseenter : function(){
-                    $this = $(this);
-                    $this.addClass("addHeader");
+                    _header.addClass("addHeader");
                 },
                 mouseleave : function(){
-                    $this = $(this);
-                    if( $scroll === false ){ //스크롤 논리값이 false일때만 removeClass하고 아니라면 마우스리브를 해도 절대 removeClass하지 말아라
-                        $this.removeClass("addHeader");
+                    if( t===true ){
+                        _header.removeClass("addHeader");
                     }
                 }
-            });
+            })
 
-            //휠마우스이벤트
-            $window.scroll(function(){
-                $this = $(this);
-                if( $this.scrollTop() >= 30 ){
-                    $scroll = true; //스크롤 30px 이상인 경우 true로 변경하라
-                    $header.addClass("addHeader"); 
+            _window.scroll(function(){
+                if( _window.scrollTop() >= 30 ){
+                    t = false;
+                    _header.addClass("addHeader");
                 }
                 else{
-                    $scroll = false; //스크롤 30px 이하인 경우 true로 변경하라
-                    $header.removeClass("addHeader"); 
+                    t = true;
+                    _header.removeClass("addHeader");
                 }
-            });
-
+            })
         },
         section1Fn : function(){
-            var cnt = 0;
-            var n = $(".slide").length-2;
-            var $nextBtn = $(".next-btn");
-            var $prevBtn = $(".prev-btn");
-            var $slideWrap = $(".slide-wrap");
 
-            setTimeout(autoTimerFn, 100);
+            var _cnt = 0;
+            var _slideWrap = $(".slide-wrap");
+            var _setId = 0;
+            var n = $(".slide").length;
+            var _nextBtn = $(".next-btn");
+            var _prevBtn = $(".prev-btn");
 
-            function autoTimerFn(){
-                setId = setInterval(nextCountFn,3000);
+            setTimeout(autoPlayFn, 10);
+            function autoPlayFn(){
+                _setId = setInterval(nextSlideFn, 3000)
             }
 
             function mainSlideFn(){
-                $slideWrap.stop().animate({ left: -(100*cnt) +"%" },600,function(){
-                    if(cnt>n-1)cnt=0;
-                    if(cnt<0)cnt=n-1;
-                    $slideWrap.stop().animate({ left: -(100*cnt) +"%" },0)
+                _slideWrap.stop().animate({ left : -(100*_cnt) +"%" },600,function(){
+                    if(_cnt>n)_cnt=0;
+                    if(_cnt<0)_cnt=n;
+                    _slideWrap.stop().animate({ left : -(100*_cnt) +"%" },0);
                 });
             }
 
-            function nextCountFn(){
-                cnt++;
-                mainSlideFn();
-            };
-
-            function prevCountFn(){
-                cnt--;
+            function nextSlideFn(){
+                _cnt++;
                 mainSlideFn();
             }
 
-            $nextBtn.on({
-               click : function(){
-                   if( !$slideWrap.is(":animated") ){
-                       nextCountFn();
-                   }
-               } 
+            function prevSlideFn(){
+                _cnt--;
+                mainSlideFn();
+            }
+
+            _nextBtn.on({
+                click : function(){
+                    if( !_slideWrap.is(":animated") ){
+                        nextSlideFn();
+                    }
+                }
+            });
+            _prevBtn.on({
+                click : function(){
+                    if( !_slideWrap.is(":animated") ){
+                        prevSlideFn();
+                    }
+                }
             });
 
-            $prevBtn.on({
-                click : function(){
-                    if( !$slideWrap.is(":animated") ){
-                        prevCountFn();
-                    }
-                } 
-             });
 
         },
         section2Fn : function(){
-            
+
         },
         section3Fn : function(){
-            
+
         },
         section4Fn : function(){
-            
+
         },
         section5Fn : function(){
-            
+
         },
         footerFn : function(){
-                
+
         }
     }
-
-    jason.init();
+    agency.init();
 })(jQuery,window,document,);
